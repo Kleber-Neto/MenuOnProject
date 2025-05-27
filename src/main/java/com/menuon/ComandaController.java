683b144bba2bbcfd04cfe845a824838c628e9285
Kleber Neto <kleberneto29@gmail.com>
2025-05-27 00:48:56 -0300

    ppp

--- src/main/java/com/menuon/ComandaController.java
+++ src/main/java/com/menuon/ComandaController.java
@@ -52,18 +52,19 @@ public class ComandaController {
         return repository.save(c);
     }
 
-    // ✅ Atualizar SÓ o STATUS (Pago / Em Aberto)
-    @PutMapping("/{id}/status")
-    public Comanda atualizarStatus(@PathVariable Long id, @RequestBody Map<String, String> body) {
-        Comanda c = repository.findById(id).orElseThrow();
+    // // ✅ Atualizar SÓ o STATUS (Pago / Em Aberto)
+    // @PutMapping("/{id}/status")
+    // public Comanda atualizarStatus(@PathVariable Long id, @RequestBody
+    // Map<String, String> body) {
+    // Comanda c = repository.findById(id).orElseThrow();
 
-        String novoStatus = body.get("status");
-        if (novoStatus != null) {
-            c.setStatus(novoStatus);
-        }
+    // String novoStatus = body.get("status");
+    // if (novoStatus != null) {
+    // c.setStatus(novoStatus);
+    // }
 
-        return repository.save(c);
-    }
+    // return repository.save(c);
+    // }
 
     @DeleteMapping("/{id}")
     public void excluir(@PathVariable Long id) {
